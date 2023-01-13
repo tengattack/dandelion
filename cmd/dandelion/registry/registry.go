89830475b417132fd0c4f69d809487b540e2ce71
tengattack <tengattack@gmail.com>
2023-01-13 11:08:34 +0800

    Add rider & nyx api

--- cmd/dandelion/registry/registry.go
+++ cmd/dandelion/registry/registry.go
@@ -1,14 +1,18 @@
 package registry
 
 import (
+	"bytes"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/tengattack/dandelion/client"
 	"github.com/tengattack/dandelion/cmd/dandelion/config"
@@ -22,6 +26,9 @@ type Client struct {
 	password   string
 	verify     bool
 	httpClient *http.Client
+
+	accessToken string
+	authTime    time.Time
 }
 
 // ListTagsResponse {catalog}/tags/list response
@@ -33,6 +40,24 @@ type ListTagsResponse struct {
 type harborTag struct {
 	Name    string `json:"name"`
 	Created string `json:"created"`
+}
+
+type riderTag struct {
+	Branch          string `json:"branch"`
+	BuildTaskID     int64  `json:"build_task_id"`
+	BuildTime       string `json:"build_time"`
+	CommitID        string `json:"commit_id"`
+	CommitMessage   string `json:"commit_message"`
+	DockerImageName string `json:"docker_image_name"`
+
+	RetagDockerImageName *string `json:"retag_docker_image_name"`
+}
+
+type riderTagListResponse struct {
+	Data        []riderTag `json:"data"`
+	Result      bool       `json:"result"`
+	TotalPages  int64      `json:"totalPages"`
+	TotalCounts int64      `json:"total_counts"`
 }
 
 // NewClient creates a new registry client
@@ -165,9 +190,188 @@ func (c *Client) harborListTags(catalog string) (*ListTagsResponse, error) {
 	return v, nil
 }
 
+func (c *Client) riderListTags(catalog string) (*ListTagsResponse, error) {
+	url := fmt.Sprintf("%s/api/pkg/search_app_release", c.endpoint)
+	query := make(map[string]interface{})
+	query["tree_path"] = "bilibili." + catalog
+	queryBuf, err := json.Marshal(query)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(queryBuf))
+	if err != nil {
+		return nil, err
+	}
+	client.InitHTTPRequest(req, false)
+	req.Header.Set("Content-Type", "application/json")
+	if c.password != "" {
+		req.Header.Set("rider-token", c.password)
+	}
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var resp riderTagListResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+	v := &ListTagsResponse{
+		Name: catalog,
+		Tags: make([]string, 0, len(resp.Data)),
+	}
+	if len(resp.Data) > 0 {
+		for _, tag := range resp.Data {
+			imageName := tag.DockerImageName
+			if tag.RetagDockerImageName != nil && *tag.RetagDockerImageName != "" {
+				imageName = *tag.RetagDockerImageName
+			}
+			pos := strings.LastIndex(imageName, ":")
+			if pos < 0 {
+				continue
+			}
+			v.Tags = append(v.Tags, imageName[pos+1:])
+		}
+	}
+	return v, nil
+}
+
+func (c *Client) nyxRenewAccessToken() error {
+	api := fmt.Sprintf("%s/ep/admin/nyx/ep-auth", c.endpoint)
+	query := url.Values{}
+	if c.password != "" {
+		parts := strings.SplitN(c.password, ":", 2)
+		if len(parts) != 2 {
+			return errors.New("malformed auth password")
+		}
+		query.Set("client_id", parts[0])
+		query.Set("client_secret", parts[1])
+	}
+	req, err := http.NewRequest(http.MethodGet, api+"?"+query.Encode(), nil)
+	if err != nil {
+		return err
+	}
+	client.InitHTTPRequest(req, false)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("http status %d", res.StatusCode)
+	}
+	var resp struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Data    string `json:"data"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return fmt.Errorf("json unmarshal error: %v", err)
+	}
+	if resp.Code != 0 {
+		return fmt.Errorf("api error: %d - %s", resp.Code, resp.Message)
+	}
+	if resp.Data == "" {
+		return errors.New("invalid access token")
+	}
+	c.accessToken = resp.Data
+	c.authTime = time.Now()
+	return nil
+}
+
+func (c *Client) nyxListTags(catalog string) (*ListTagsResponse, error) {
+	now := time.Now()
+	if c.accessToken == "" || c.authTime.Before(now.Add(-8*time.Hour)) {
+		// renew access token
+		err := c.nyxRenewAccessToken()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	api := fmt.Sprintf("%s/ep/admin/nyx/open/build/images", c.endpoint)
+	query := url.Values{}
+	query.Set("appid", catalog)
+	req, err := http.NewRequest(http.MethodGet, api+"?"+query.Encode(), nil)
+	if err != nil {
+		return nil, err
+	}
+	client.InitHTTPRequest(req, false)
+	req.Header.Set("Content-Type", "application/json")
+	if c.username != "" {
+		req.Header.Set("Perm-Code", c.username)
+	}
+	req.Header.Set("Access-Token", c.accessToken)
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http status %d", res.StatusCode)
+	}
+	var resp struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+		Data    struct {
+			Items []riderTag `json:"items"`
+		} `json:"data"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("json unmarshal error: %v", err)
+	}
+	if resp.Code != 0 {
+		if resp.Code == -401 {
+			// reset auth token
+			c.accessToken = ""
+		}
+		return nil, fmt.Errorf("api error: %d - %s", resp.Code, resp.Message)
+	}
+	v := &ListTagsResponse{
+		Name: catalog,
+		Tags: make([]string, 0, len(resp.Data.Items)),
+	}
+	if len(resp.Data.Items) > 0 {
+		for _, item := range resp.Data.Items {
+			imageName := item.DockerImageName
+			if item.RetagDockerImageName != nil && *item.RetagDockerImageName != "" {
+				imageName = *item.RetagDockerImageName
+			}
+			pos := strings.LastIndex(imageName, ":")
+			if pos < 0 {
+				continue
+			}
+			v.Tags = append(v.Tags, imageName[pos+1:])
+		}
+	}
+	return v, nil
+}
+
 // ListTags list tags for catalog
 func (c *Client) ListTags(catalog string) (*ListTagsResponse, error) {
-	if c.service == "harbor" {
+	if c.service == "rider" {
+		return c.riderListTags(catalog)
+	} else if c.service == "nyx" {
+		return c.nyxListTags(catalog)
+	} else if c.service == "harbor" {
 		return c.harborListTags(catalog)
 	}
 	// defaults to registry
